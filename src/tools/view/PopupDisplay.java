c7266525130c1153c461a81aef7aaaae88cab5df
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-10-03 12:42:02 -0600

    Created @author files and java docs for the methods as well as added comments and notes about the files.

--- src/tools/view/PopupDisplay.java
+++ src/tools/view/PopupDisplay.java
@@ -2,13 +2,32 @@ package tools.view;
 
 import javax.swing.JOptionPane;
 
-public class PopupDisplay
+//	/** + enter (above the class) - creates the @author file where you can add things to like @version with the version and date
+/**
+ * Simple GUI class using JOptionPane
+ * @author Kashish Singh
+ *	@version 1.0 03.10.2017
+ */
+	
+public class PopupDisplay	//right click + refactor + rename = allows you to rename classes, projects, etc.
 {
+	/*	/** + enter - creates the java doc for that method
+	Use full sentences and be informative */
+	
+	/**
+	 * Displays a popup showing the supplied String to the user.
+	 * @param	textToDisplay The xt to be displayed
+	 */
 	public void displayText(String textToDisplay)
 	{
 		JOptionPane.showMessageDialog(null, textToDisplay);
 	}
 	
+	/**
+	 * Displays a popup to display a question to the user
+	 * @param	fromQuestion The question being asked
+	 * @return	The user's response to the questions
+	 */
 	public String getResponse(String fromQuestion)
 	{
 		String answer = "";
