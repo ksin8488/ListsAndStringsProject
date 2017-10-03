ab649009878b4b85c5a7dcd9201196798242509a
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-10-03 13:19:13 -0600

    Added the list and array list imports as well as use them to create a kahoot list with different values assigned. Then a constructor was made along with the values in the model. Then created multiple Kahoots with creator names and questinon numbersdded into them then add them all to the myKahoots List that we created.

--- src/tools/controller/ListController.java
+++ src/tools/controller/ListController.java
@@ -1,9 +1,35 @@
 package tools.controller;
 
+import java.util.List;
+import java.util.ArrayList;
+import tools.model.Kahoot;		//must add because Kahoot class is in a different file
+
 public class ListController
 {
+	private List<Kahoot> myKahoots;
+	
+	public ListController()
+	{
+		myKahoots = new ArrayList<Kahoot>();		//use array list instead because using list is too vague
+	}
+	
 	public void start()
 	{
-		
+		Kahoot myFirstKahoot = new Kahoot();		//new class constructor
+		myKahoots.add(myFirstKahoot);
+	}
+	
+	private void FillTheList()		//private helper method
+	{
+		Kahoot fiftyStates = new Kahoot("Kashish", 50);		//make a kahoot called fiftyStates that is made by Kashish with 50 questions
+		Kahoot mySecondKahoot = new Kahoot("Ethan", 2);
+		Kahoot bigQuiz = new Kahoot("Derek", Integer.MAX_VALUE);		//DEREK WHY WOULD YOU DO THIS?!?! (maximum of int is around 2147483647)
+		Kahoot animalColor = new Kahoot("Branton", 10);
+		Kahoot presidents = new Kahoot("Obama", 44);
+		myKahoots.add(fiftyStates);	
+		myKahoots.add(mySecondKahoot);
+		myKahoots.add(bigQuiz);
+		myKahoots.add(animalColor);
+		myKahoots.add(presidents);
 	}
 }
---
+++ src/tools/model/Kahoot.java
@@ -0,0 +1,23 @@
+package tools.model;
+
+public class Kahoot
+{
+	private int questionCount;		//number of questions
+	private int level;				//level of difficulty
+	private String creator;			//Name of the creator of the Kahoot
+	private boolean isJumble; 		//are the questions mixed up? True or False.
+	
+	public Kahoot()	//constructor is visibility (usually public) and then the class name with ()
+	{
+		this.questionCount = 0;
+		this.level = 1;
+		this.creator = "nobody";
+		this.isJumble = false;
+	}
+	
+	public Kahoot(String creator, int questionCount)
+	{
+		this.creator = creator;
+		this.questionCount = questionCount;
+	}
+}
